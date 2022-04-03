0b5ed9d668755a8ffd8bbe45aae5e60b173279a4
Vignesh <vigneshravi7117@gmail.com>
2022-04-04 00:20:52 +0530

    Duration of animation extended

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -18,7 +18,7 @@ class SplashScreenState extends State<Splash>
     super.initState();
 
     _controller = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 3000));
+        vsync: this, duration: const Duration(milliseconds: 5000));
 
     Timer(
         const Duration(seconds: 3),

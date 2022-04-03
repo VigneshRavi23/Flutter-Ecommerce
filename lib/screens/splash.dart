1653541bcdab001c234dcc6f1b5a97a654df2a9f
Vignesh <vigneshravi7117@gmail.com>
2022-04-04 00:06:03 +0530

    Splash Screen Finihed

---
+++ lib/screens/splash.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+import 'package:flipzon/screens/home.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+  @override
+  SplashScreenState createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<Splash>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 3000));
+
+    Timer(
+        const Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => const Home())));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Center(
+          child: Lottie.asset('assets/lottie/shopping.json',
+              controller: _controller, onLoaded: (comp) {
+        _controller.duration = comp.duration;
+        _controller.forward();
+      })),
+    );
+  }
+}

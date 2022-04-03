207292b84c5be77cc5fe7b87ee97e2eb367cd8be
Vignesh <vigneshravi7117@gmail.com>
2022-04-04 00:18:49 +0530

    Splash removed unused import file

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:flipzon/screens/home.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 

b54ac4e3ae202dfe793d2c38e70bb765ec56f14a
Vignesh <vigneshravi7117@gmail.com>
2022-04-09 23:52:12 +0530

    Storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flipzon/screens/splash.dart';
 import 'package:flutter/material.dart';
-import 'package:get_storage/get_storage.dart';
 
 void main() async {
-  await GetStorage.init();
   runApp(const MyApp());
 }
 
